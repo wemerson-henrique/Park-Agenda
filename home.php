57cb006c8660aa8c57e869bed240c12b022b9190
tulioo2001 <99624427+tulioo2001@users.noreply.github.com>
2022-02-15 23:11:02 -0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,39 @@
+<?php 
+    include('conf-bootstrap-web.php');
+?>
+<div class="card-body">
+    <div class="row">
+        <figure class="figure">
+            <img src="imagens/logParkAgenda.png" class="figure-img img-fluid rounded" 
+            alt="Log do site Park Agenda.
+             A logo é semelhante um trevo e tem escrito no topo Park Agenda" 
+             align="center" rele="alert">
+        </figure>
+    </div>
+    <div class="row">
+        <div class="col-4">
+            <div class="card bg-dark text-white">
+                <img class="card-img" src="imagens/img-map.png" alt="Imagem do card">
+                <div class="card-img-overlay">
+                    <h5 class="card-title">Mapa</h5>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card bg-dark text-white">
+                <img class="card-img" src="imagens/administracao.jpg" alt="Imagem do card">
+                <div class="card-img-overlay">
+                    <h5 class="card-title">Administração</h5>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card bg-dark text-white">
+                <img class="card-img" src="imagens/boletins.png" alt="Imagem do card">
+                <div class="card-img-overlay">
+                    <h5 class="card-title">Boletins</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
