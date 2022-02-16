957490459f7180bcf3630eb0ac13451ae477753e
Wemerson Henrique <68334448+wemerson-henrique@users.noreply.github.com>
2022-02-16 15:55:03 -0300

    .

--- view/footer.php
+++ view/footer.php
@@ -1,7 +1,6 @@
 <footer>
         <div class="alert alert-secondary" align="center" role="alert" 
         style="background-color: #191970; color: #fff;">
-          <p><a class="btn btn-dark" href="#topo">Voltar</a></p>
           <p> Desenvolvido por Túlio e Wemerson</p>
           <p> Direitos Reservados, Copywriter</p>
         </div>
